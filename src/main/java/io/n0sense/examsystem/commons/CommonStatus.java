26b66ce8ea311b3040b0419a5986de12b9757445
n0sense <1436648676@qq.com>
2022-11-23 19:34:18 +0800

    add common constants interface

---
+++ src/main/java/io/n0sense/examsystem/commons/CommonStatus.java
@@ -0,0 +1,8 @@
+package io.n0sense.examsystem.commons;
+
+public interface CommonStatus {
+    int OK = 0;
+    int ERR_USERNAME_IN_USE = 1001;
+    int ERR_INCORRECT_PASSWORD = 1002;
+    int ERR_USER_NOT_FOUND = 1003;
+}
