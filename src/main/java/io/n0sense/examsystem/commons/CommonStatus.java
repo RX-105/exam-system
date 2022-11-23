a48e0aa8a97782206dd509cacb0a309bdc8fa6af
n0sense <1436648676@qq.com>
2022-11-23 22:50:43 +0800

    add documentation

--- src/main/java/io/n0sense/examsystem/commons/CommonStatus.java
+++ src/main/java/io/n0sense/examsystem/commons/CommonStatus.java
@@ -1,5 +1,10 @@
 package io.n0sense.examsystem.commons;
 
+/**
+ * 通用信息类。
+ * @author kazuha
+ * @since 2022/11/23
+ */
 public interface CommonStatus {
     int OK = 0;
     int ERR_USERNAME_IN_USE = 1001;
