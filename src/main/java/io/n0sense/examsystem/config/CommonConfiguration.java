977015f62d48ac555c549634dc7b6a744bfa6d2d
n0sense <1436648676@qq.com>
2022-11-23 22:50:55 +0800

    add favicon config

---
+++ src/main/java/io/n0sense/examsystem/config/CommonConfiguration.java
@@ -0,0 +1,34 @@
+package io.n0sense.examsystem.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
+import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
+
+import java.util.Collections;
+import java.util.List;
+
+@Configuration
+public class CommonConfiguration {
+    @Bean
+    public SimpleUrlHandlerMapping customFaviconHandlerMapping() {
+        SimpleUrlHandlerMapping mapping = new SimpleUrlHandlerMapping();
+        mapping.setOrder(Integer.MIN_VALUE);
+        mapping.setUrlMap(Collections.singletonMap(
+                "/favicon.ico", faviconRequestHandler()));
+        return mapping;
+    }
+
+    @Bean
+    protected ResourceHttpRequestHandler faviconRequestHandler() {
+        ResourceHttpRequestHandler requestHandler
+                = new ResourceHttpRequestHandler();
+        ClassPathResource classPathResource
+                = new ClassPathResource("static/favicon.ico");
+        List<Resource> locations = List.of(classPathResource);
+        requestHandler.setLocations(locations);
+        return requestHandler;
+    }
+}
