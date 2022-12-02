5bd26a5d3ea33ce3463cac3c97911ffd68a793f9
n0sense <1436648676@qq.com>
2022-12-02 23:53:44 +0800

    extend BaseRepository

--- src/main/java/io/n0sense/examsystem/repository/ExamRepository.java
+++ src/main/java/io/n0sense/examsystem/repository/ExamRepository.java
@@ -3,5 +3,5 @@ package io.n0sense.examsystem.repository;
 import io.n0sense.examsystem.entity.Exam;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ExamRepository extends JpaRepository<Exam, Long> {
+public interface ExamRepository extends JpaRepository<Exam, Long>, BaseRepository<Exam> {
 }
