6fc791bc99ca94f41b39d3b77dcd8b9ebf422e06
n0sense <1436648676@qq.com>
2022-12-02 23:28:51 +0800

    add more status

--- src/main/java/io/n0sense/examsystem/commons/CommonStatus.java
+++ src/main/java/io/n0sense/examsystem/commons/CommonStatus.java
@@ -10,4 +10,8 @@ public interface CommonStatus {
     int ERR_USERNAME_IN_USE = 1001;
     int ERR_INCORRECT_PASSWORD = 1002;
     int ERR_USER_NOT_FOUND = 1003;
+    int ERR_MAJOR_EXISTS = 1004;
+    int ERR_MAJOR_NOT_FOUND = 1005;
+    int ERR_EXAM_EXISTS = 1006;
+    int ERR_EXAM_NOT_FOUND = 1007;
 }
