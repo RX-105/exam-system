0dc68b2608b4b16042db9af672e4b1138bc49509
n0sense <1436648676@qq.com>
2022-12-02 23:53:06 +0800

    implement register, login, addEnrollmentInfo methods

--- src/main/java/io/n0sense/examsystem/controller/AdminController.java
+++ src/main/java/io/n0sense/examsystem/controller/AdminController.java
@@ -1,6 +1,7 @@
 package io.n0sense.examsystem.controller;
 
 import io.n0sense.examsystem.commons.CommonStatus;
+import io.n0sense.examsystem.entity.Admin;
 import io.n0sense.examsystem.entity.ResponseEntity;
 import io.n0sense.examsystem.service.AdminService;
 import io.n0sense.examsystem.util.IpUtil;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDateTime;
 
 @Controller
 @RequestMapping("/api")
@@ -23,34 +25,58 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
     private final Logger logger = LoggerFactory.getLogger(AdminController.class);
-    @PostMapping("/register")
-    public ModelAndView register(String username, String password, String groupName, HttpServletRequest request, Model model){
-        int result = adminService.register(username, password, groupName, IpUtil.getIpAddress(request));
-        if (result == CommonStatus.OK){
+    @PostMapping({"/register"})
+    public ModelAndView register(String username, String password, String groupName, HttpServletRequest request, Model model) {
+        int result = this.adminService.register(username, password, groupName, IpUtil.getIpAddress(request));
+        if (result == CommonStatus.OK) {
             request.getSession().setAttribute("username", username);
-            return new ModelAndView("index");
-        } else if (result == CommonStatus.ERR_USERNAME_IN_USE){
+            request.getSession().setAttribute("group", groupName);
+            request.getSession().setAttribute("role", "admin");
+            return new ModelAndView("/admin/home");
+        } else if (result == CommonStatus.ERR_USERNAME_IN_USE) {
             model.addAttribute("msg", "用户名已占用。");
-            return new ModelAndView("register");
+            return new ModelAndView("/admin/register");
         } else {
-            logger.error("register: Unresolved result "+result);
+            this.logger.error("register: Unresolved result " + result);
             return new ModelAndView("404");
-
         }
     }
-    @PostMapping("/login")
-    public ModelAndView login(String username, String password, Model model){
-        int result = adminService.login(username, password);
-        if (result == CommonStatus.OK){
-            return new ModelAndView("index");
+    @PostMapping({"/login"})
+    public ModelAndView login(String username, String password, Model model, HttpServletRequest request) {
+        int result = this.adminService.login(username, password);
+        Admin admin = (Admin)this.adminService.findByName(username).get();
+        if (result == 0) {
+            request.getSession().setAttribute("username", username);
+            request.getSession().setAttribute("group", admin.getGroupName());
+            request.getSession().setAttribute("role", "admin");
+            model.addAttribute("role", "admin");
+            return new ModelAndView("/admin/home");
         } else if (result == CommonStatus.ERR_INCORRECT_PASSWORD) {
             model.addAttribute("msg", "密码错误。");
-            return new ModelAndView("login");
-        } else if (result == CommonStatus.ERR_USER_NOT_FOUND){
+            return new ModelAndView("/admin/login");
+        } else if (result == CommonStatus.ERR_USER_NOT_FOUND) {
             model.addAttribute("msg", "没有这个用户。");
-            return new ModelAndView("login");
+            return new ModelAndView("/admin/login");
         } else {
-            logger.error("login: Unresolved result "+result);
+            this.logger.error("login: Unresolved result " + result);
+            return new ModelAndView("404");
+        }
+    }
+
+    @PostMapping({"/publish"})
+    public ModelAndView publishEnrollmentInfo(String majorName, int applicant, int enrollment, int admission, double score, String examName, LocalDateTime start, LocalDateTime end, Model model) {
+        int result = this.adminService.addEnrollmentInfo(majorName, applicant, enrollment, admission, score, examName, start, end);
+        if (result == CommonStatus.ERR_MAJOR_EXISTS) {
+            model.addAttribute("msg", "已经有这个专业了。");
+            return new ModelAndView("/admin/recruit/recruit-maintain");
+        } else if (result == CommonStatus.ERR_EXAM_EXISTS) {
+            model.addAttribute("msg", "已经有这项考试了。");
+            return new ModelAndView("/admin/recruit/recruit-maintain");
+        } else if (result == CommonStatus.OK) {
+            model.addAttribute("msg", "添加完成。");
+            return new ModelAndView("/admin/recruit/recruit-maintain");
+        } else {
+            this.logger.error("publishEnrollmentInfo: Unresolved result " + result);
             return new ModelAndView("404");
         }
     }
