6ed7e3936be5aab21e72828da679e08744bc86c9
n0sense <1436648676@qq.com>
2022-12-02 23:55:24 +0800

    add GroupConstants for constants of groupNames

---
+++ src/main/java/io/n0sense/examsystem/commons/GroupConstants.java
@@ -0,0 +1,8 @@
+package io.n0sense.examsystem.commons;
+
+public interface GroupConstants {
+    String STUDENT = "student";
+    String SYSTEM_ADMIN = "sudoers";
+    String ACADEMIC_AFFAIRS = "aca-aff";
+    String RECRUIT_AFFAIRS = "recruit";
+}
