9de2d3cf3326b3692c39e3e7f9a756698ae32991
n0sense <1436648676@qq.com>
2022-12-03 11:59:15 +0800

    rename constants

--- src/main/java/io/n0sense/examsystem/commons/GroupConstants.java
+++ src/main/java/io/n0sense/examsystem/commons/GroupConstants.java
@@ -1,8 +1,10 @@
 package io.n0sense.examsystem.commons;
 
 public interface GroupConstants {
-    String STUDENT = "student";
-    String SYSTEM_ADMIN = "sudoers";
-    String ACADEMIC_AFFAIRS = "aca-aff";
-    String RECRUIT_AFFAIRS = "recruit";
+    String ROLE_STUDENT = "student";
+    String ROLE_ADMIN = "admin";
+
+    String GROUP_SYSTEM_ADMIN = "sudoers";
+    String GROUP_ACADEMIC_AFFAIRS = "aca-aff";
+    String GROUP_RECRUIT_AFFAIRS = "recruit";
 }
