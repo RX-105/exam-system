136b852b794ea1f496a83192245b2c93e799e7b9
n0sense <1436648676@qq.com>
2022-12-03 12:01:02 +0800

    switch to GroupConstants

--- src/main/java/io/n0sense/examsystem/controller/AdminController.java
+++ src/main/java/io/n0sense/examsystem/controller/AdminController.java
@@ -1,6 +1,7 @@
 package io.n0sense.examsystem.controller;
 
 import io.n0sense.examsystem.commons.CommonStatus;
+import io.n0sense.examsystem.commons.GroupConstants;
 import io.n0sense.examsystem.entity.Admin;
 import io.n0sense.examsystem.service.impl.AdminService;
 import io.n0sense.examsystem.util.IpUtil;
@@ -22,38 +23,39 @@ public class AdminController {
     @Autowired
     private AdminService adminService;
     private final Logger logger = LoggerFactory.getLogger(AdminController.class);
+
     @PostMapping({"/register"})
     public ModelAndView register(String username, String password, String groupName, HttpServletRequest request, Model model) {
         int result = this.adminService.register(username, password, groupName, IpUtil.getIpAddress(request));
         if (result == CommonStatus.OK) {
             request.getSession().setAttribute("username", username);
             request.getSession().setAttribute("group", groupName);
-            request.getSession().setAttribute("role", "admin");
-            return new ModelAndView("/admin/home");
+            request.getSession().setAttribute("role", GroupConstants.ROLE_ADMIN);
+            return new ModelAndView("/" + GroupConstants.ROLE_ADMIN + "/home");
         } else if (result == CommonStatus.ERR_USERNAME_IN_USE) {
             model.addAttribute("msg", "用户名已占用。");
-            return new ModelAndView("/admin/register");
+            return new ModelAndView("/" + GroupConstants.ROLE_ADMIN + "/home");
         } else {
             this.logger.error("register: Unresolved result " + result);
             return new ModelAndView("404");
         }
     }
+
     @PostMapping({"/login"})
     public ModelAndView login(String username, String password, Model model, HttpServletRequest request) {
         int result = this.adminService.login(username, password);
-        Admin admin = (Admin)this.adminService.findByName(username).get();
+        Admin admin = (Admin) this.adminService.findByName(username).get();
         if (result == 0) {
             request.getSession().setAttribute("username", username);
             request.getSession().setAttribute("group", admin.getGroupName());
-            request.getSession().setAttribute("role", "admin");
-            model.addAttribute("role", "admin");
-            return new ModelAndView("/admin/home");
+            request.getSession().setAttribute("role", GroupConstants.ROLE_ADMIN);
+            return new ModelAndView("/" + GroupConstants.ROLE_ADMIN + "/home");
         } else if (result == CommonStatus.ERR_INCORRECT_PASSWORD) {
             model.addAttribute("msg", "密码错误。");
-            return new ModelAndView("/admin/login");
+            return new ModelAndView("/" + GroupConstants.ROLE_ADMIN + "/home");
         } else if (result == CommonStatus.ERR_USER_NOT_FOUND) {
             model.addAttribute("msg", "没有这个用户。");
-            return new ModelAndView("/admin/login");
+            return new ModelAndView("/" + GroupConstants.ROLE_ADMIN + "/home");
         } else {
             this.logger.error("login: Unresolved result " + result);
             return new ModelAndView("404");
@@ -65,13 +67,16 @@ public class AdminController {
         int result = this.adminService.addEnrollmentInfo(majorName, applicant, enrollment, admission, score, examName, start, end);
         if (result == CommonStatus.ERR_MAJOR_EXISTS) {
             model.addAttribute("msg", "已经有这个专业了。");
-            return new ModelAndView("/admin/recruit/recruit-maintain");
+            return new ModelAndView("/" + GroupConstants.ROLE_ADMIN +
+                    "/" + GroupConstants.GROUP_RECRUIT_AFFAIRS + "/recruit-maintain");
         } else if (result == CommonStatus.ERR_EXAM_EXISTS) {
             model.addAttribute("msg", "已经有这项考试了。");
-            return new ModelAndView("/admin/recruit/recruit-maintain");
+            return new ModelAndView("/" + GroupConstants.ROLE_ADMIN +
+                    "/" + GroupConstants.GROUP_RECRUIT_AFFAIRS + "/recruit-maintain");
         } else if (result == CommonStatus.OK) {
             model.addAttribute("msg", "添加完成。");
-            return new ModelAndView("/admin/recruit/recruit-maintain");
+            return new ModelAndView("/" + GroupConstants.ROLE_ADMIN +
+                    "/" + GroupConstants.GROUP_RECRUIT_AFFAIRS + "/recruit-maintain");
         } else {
             this.logger.error("publishEnrollmentInfo: Unresolved result " + result);
             return new ModelAndView("404");
