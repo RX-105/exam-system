fcbe10a097264df3e64fe0ec7821c7001b7ec685
n0sense <1436648676@qq.com>
2022-12-03 09:20:43 +0800

    exclude static resource folder

--- src/main/java/io/n0sense/examsystem/controller/PageController.java
+++ src/main/java/io/n0sense/examsystem/controller/PageController.java
@@ -9,17 +9,17 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class PageController {
-    @GetMapping("/{path}")
+    @GetMapping("/{path:^.*(?!static)}")
     public String page(@PathVariable String path) {
         return path;
     }
 
-    @GetMapping("/{path1}/{path2}")
+    @GetMapping("/{path1:^.*(?!static)}/{path2}")
     public String page(@PathVariable String path1, @PathVariable String path2) {
         return path1 + "/" + path2;
     }
 
-    @GetMapping({"/{path1}/{path2}/{path3}"})
+    @GetMapping({"/{path1:^.*(?!static)}/{path2}/{path3}"})
     public String page(@PathVariable String path1, @PathVariable String path2, @PathVariable String path3) {
         return path1 + "/" + path2 + "/" + path3;
     }
