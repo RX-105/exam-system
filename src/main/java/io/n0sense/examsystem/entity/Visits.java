1cdaa82647cee36fe92efc0c36bdbba169a46f02
n0sense <1436648676@qq.com>
2022-12-31 19:09:28 +0800

    add visits entity to record website visit count

---
+++ src/main/java/io/n0sense/examsystem/entity/Visits.java
@@ -0,0 +1,22 @@
+package io.n0sense.examsystem.entity;
+
+import lombok.*;
+import org.hibernate.annotations.SQLInsert;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "visits")
+public class Visits {
+    @Id
+    private LocalDate time;
+    private Long count;
+}
