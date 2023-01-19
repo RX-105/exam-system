483900569f0eb6c849c1cff2fbfa337d75ca4d98
n0sense <1436648676@qq.com>
2023-01-19 10:36:51 +0800

    add LogService interface and implementations

---
+++ src/main/java/io/n0sense/examsystem/service/ILogService.java
@@ -0,0 +1,15 @@
+package io.n0sense.examsystem.service;
+
+import io.n0sense.examsystem.entity.Log;
+import org.springframework.data.domain.Page;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.LocalDate;
+
+public interface ILogService {
+    void recordLogin(HttpServletRequest request);
+    Page<Log> getUserLogins(String username, int page, int size);
+    Page<Log> getGroupLogin(String groupName, int page, int size);
+    Page<Log> queryLogByTimeRange(String username, int page, int size,
+                                  LocalDate from, LocalDate to);
+}
