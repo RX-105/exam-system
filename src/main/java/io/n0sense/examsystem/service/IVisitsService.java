f49138596a1f0070b349fa7dd07c2c6af4ce813a
n0sense <1436648676@qq.com>
2023-01-19 11:11:18 +0800

    add VisitsService interface and implementations

---
+++ src/main/java/io/n0sense/examsystem/service/IVisitsService.java
@@ -0,0 +1,7 @@
+package io.n0sense.examsystem.service;
+
+public interface IVisitsService {
+    Long getTodayVisits();
+    Long getTotalVisits();
+    Long getTotalUsers();
+}
---
+++ src/main/java/io/n0sense/examsystem/service/impl/VisitsService.java
@@ -0,0 +1,54 @@
+package io.n0sense.examsystem.service.impl;
+
+import io.n0sense.examsystem.entity.Visits;
+import io.n0sense.examsystem.repository.VisitsRepository;
+import io.n0sense.examsystem.service.IVisitsService;
+import io.n0sense.examsystem.util.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.time.LocalDate;
+import java.util.Optional;
+
+@Service
+public class VisitsService implements IVisitsService {
+    private final VisitsRepository visitsRepository;
+    private final Session session;
+
+    @Autowired
+    public VisitsService(HibernateUtil hibernateUtil, VisitsRepository visitsRepository){
+        this.session = hibernateUtil.getSessionJavaConfigFactory().openSession();
+        this.visitsRepository = visitsRepository;
+    }
+
+    @Override
+    public Long getTodayVisits() {
+        Optional<Visits> optionalTodayVisit = visitsRepository.findById(LocalDate.now());
+        Visits todayVisit = optionalTodayVisit.orElse(new Visits(LocalDate.now(), 1L));
+        return todayVisit.getCount();
+    }
+
+    @Override
+    public Long getTotalVisits() {
+        Transaction transaction = session.beginTransaction();
+        BigDecimal totalVisit = (BigDecimal) session
+                .createSQLQuery("select sum(count) from visits")
+                .getSingleResult();
+        transaction.commit();
+        return totalVisit.longValue();
+    }
+
+    @Override
+    public Long getTotalUsers() {
+        Transaction transaction = session.beginTransaction();
+        BigInteger totalUser = (BigInteger) session
+                .createSQLQuery("select count(*) from visits")
+                .getSingleResult();
+        transaction.commit();
+        return totalUser.longValue();
+    }
+}
