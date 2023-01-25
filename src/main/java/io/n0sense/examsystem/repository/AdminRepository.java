495335eadb2b88f40152d9fbab6cda4ac3f83e33
n0sense <1436648676@qq.com>
2023-01-25 22:39:29 +0800

    add method to get all admin with excluding certain group names

--- src/main/java/io/n0sense/examsystem/repository/AdminRepository.java
+++ src/main/java/io/n0sense/examsystem/repository/AdminRepository.java
@@ -1,11 +1,15 @@
 package io.n0sense.examsystem.repository;
 
 import io.n0sense.examsystem.entity.Admin;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AdminRepository extends JpaRepository<Admin, Long>, BaseRepository<Admin> {
     boolean existsAdminByName(String name);
     Optional<Admin> findAdminByName(String name);
+    Page<Admin> findAllByGroupNameIsNotContaining(String groupName, Pageable pageable);
 }
