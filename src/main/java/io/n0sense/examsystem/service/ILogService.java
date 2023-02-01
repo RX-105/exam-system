d7a60e1d3a8d9030c8a24edb1d8c07a3f343e955
n0sense <1436648676@qq.com>
2023-02-01 19:30:31 +0800

    移除新增日志方法

--- src/main/java/io/n0sense/examsystem/service/ILogService.java
+++ src/main/java/io/n0sense/examsystem/service/ILogService.java
@@ -3,11 +3,9 @@ package io.n0sense.examsystem.service;
 import io.n0sense.examsystem.entity.Log;
 import org.springframework.data.domain.Page;
 
-import jakarta.servlet.http.HttpServletRequest;
 import java.time.LocalDate;
 
 public interface ILogService {
-    void recordLogin(HttpServletRequest request);
     Page<Log> getUserLogins(String username, int page, int size);
     Page<Log> getGroupLogin(String groupName, int page, int size);
     Page<Log> queryLogByTimeRange(String username, int page, int size,
