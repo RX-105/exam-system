bd0e691a0242df105a9e89f11933c1a9df8dc6d0
n0sense <1436648676@qq.com>
2023-02-01 19:06:41 +0800

    改名为CommonConstants

--- src/main/java/io/n0sense/examsystem/commons/CommonConstants.java
+++ src/main/java/io/n0sense/examsystem/commons/CommonConstants.java
@@ -1,10 +1,12 @@
 package io.n0sense.examsystem.commons;
 
-public interface GroupConstants {
+public interface CommonConstants {
     String ROLE_STUDENT = "student";
     String ROLE_ADMIN = "admin";
 
     String GROUP_SYSTEM_ADMIN = "sudoers";
     String GROUP_ACADEMIC_AFFAIRS = "aca-aff";
     String GROUP_RECRUIT_AFFAIRS = "recruit";
+    String TABLE_NAME = "examsystem";
+    String MYSQL_HOME = "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin";
 }
