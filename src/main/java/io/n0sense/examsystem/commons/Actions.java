788e919a8b6403ea1201d349cbbf2de845e9b32d
n0sense <1436648676@qq.com>
2023-02-01 19:02:07 +0800

    添加RecordLog注解使用的常量

---
+++ src/main/java/io/n0sense/examsystem/commons/Actions.java
@@ -0,0 +1,7 @@
+package io.n0sense.examsystem.commons;
+
+public interface Actions {
+    String LOGIN = "login";
+    String CREATE_BACKUP = "create-backup";
+    String RESTORE_BACKUP = "restore-backup";
+}
