28f9f499fb4abd5059e121aababae13111027ca3
n0sense <1436648676@qq.com>
2023-02-01 19:40:28 +0800

    添加备份服务层的测试类

---
+++ src/test/java/io/n0sense/examsystem/service/TestBackupService.java
@@ -0,0 +1,32 @@
+package io.n0sense.examsystem.service;
+
+import io.n0sense.examsystem.service.impl.BackupService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
+
+@SpringBootTest
+@Transactional
+public class TestBackupService {
+    @Autowired
+    BackupService backupService;
+    @Test
+    void testDump(){
+        try {
+            backupService.dumpDatabase();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+    void testRestore(){
+        try{
+            backupService.restoreDBFromId(100010L);
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+}
