44ac74392bd37fe00576c195ae5fef8feef58041
n0sense <1436648676@qq.com>
2023-02-07 16:43:37 +0800

    改名，移动到constants包，并添加[参数缺失]错误

--- src/main/java/io/n0sense/examsystem/commons/constants/Status.java
+++ src/main/java/io/n0sense/examsystem/commons/constants/Status.java
@@ -1,15 +1,16 @@
-package io.n0sense.examsystem.commons;
+package io.n0sense.examsystem.commons.constants;
 
 /**
  * 通用信息类。
  * @author kazuha
  * @since 2022/11/23
  */
-public interface CommonStatus {
+public interface Status {
     int ERR_UNSPECIFIED = -1;
     int OK = 0;
     // 通用错误状态
     int ERR_OPERATION_FAILED = 100;
+    int ERR_PARAMETER_NOT_PRESENT = 101;
     // 详细错误状态
     int ERR_USERNAME_IN_USE = 1001;
     int ERR_INCORRECT_PASSWORD = 1002;
