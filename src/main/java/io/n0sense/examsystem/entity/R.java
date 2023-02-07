0d5cc75efa0fe63a8a7df083388a45025b8b71dd
n0sense <1436648676@qq.com>
2023-02-07 16:44:31 +0800

    添加JavaDoc

--- src/main/java/io/n0sense/examsystem/entity/R.java
+++ src/main/java/io/n0sense/examsystem/entity/R.java
@@ -8,12 +8,12 @@ import lombok.NoArgsConstructor;
 import java.util.Map;
 
 /**
- * R=Response
- * 用于Web请求中使用的响应实体类，这个类包含三个成员。
+ * R=Response<br>
+ * 用于Web请求中使用的响应实体类，这个类包含三个成员。<br>
  * status - 用于表示本次响应的状态信息。在正常情况下，统一使用0作为状态值；非正常情况返回的状态值由使用这个类的方法定义，
- *      详细参考这个方法的JavaDoc。
- * message - 用于表示本次响应的结果信息。
- * data - 用于存储具体数据的键值对对象。
+ *      详细参考这个方法的JavaDoc。<br>
+ * message - 用于表示本次响应的结果信息。<br>
+ * data - 用于存储具体数据的键值对对象。<br>
  */
 @Data
 @AllArgsConstructor
