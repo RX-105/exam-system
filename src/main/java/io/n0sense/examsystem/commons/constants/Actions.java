76862681053ccec0652f5a25859c840fc3cc42ac
n0sense <1436648676@qq.com>
2023-02-07 18:11:52 +0800

    添加注册和移除备份的常量

--- src/main/java/io/n0sense/examsystem/commons/constants/Actions.java
+++ src/main/java/io/n0sense/examsystem/commons/constants/Actions.java
@@ -2,6 +2,8 @@ package io.n0sense.examsystem.commons.constants;
 
 public interface Actions {
     String LOGIN = "login";
+    String REGISTER = "register";
     String CREATE_BACKUP = "create-backup";
     String RESTORE_BACKUP = "restore-backup";
+    String REMOVE_BACKUP = "remove-backup";
 }
