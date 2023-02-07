b78ef0d1730f5f23a72b0bc22b4915db1dfe5879
n0sense <1436648676@qq.com>
2023-02-07 16:33:35 +0800

    改名，移动到constants目录，并移除不相关常量（到application-dev.yaml）

--- src/main/java/io/n0sense/examsystem/commons/constants/Identities.java
+++ src/main/java/io/n0sense/examsystem/commons/constants/Identities.java
@@ -1,12 +1,10 @@
-package io.n0sense.examsystem.commons;
+package io.n0sense.examsystem.commons.constants;
 
-public interface CommonConstants {
+public interface Identities {
     String ROLE_STUDENT = "student";
     String ROLE_ADMIN = "admin";
 
     String GROUP_SYSTEM_ADMIN = "sudoers";
     String GROUP_ACADEMIC_AFFAIRS = "aca-aff";
     String GROUP_RECRUIT_AFFAIRS = "recruit";
-    String TABLE_NAME = "examsystem";
-    String MYSQL_HOME = "C:\\Program Files\\MySQL\\MySQL Server 8.0\\bin";
 }
