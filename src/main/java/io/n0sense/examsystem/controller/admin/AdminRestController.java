d1d183ed3db9ba35c5768cd69a243b7b665148ad
n0sense <1436648676@qq.com>
2023-02-07 16:31:58 +0800

    修改login和register方法的返回类型为R，并适配变化

--- src/main/java/io/n0sense/examsystem/controller/admin/AdminRestController.java
+++ src/main/java/io/n0sense/examsystem/controller/admin/AdminRestController.java
@@ -1,9 +1,9 @@
 package io.n0sense.examsystem.controller.admin;
 
 import io.n0sense.examsystem.annotation.RecordLog;
-import io.n0sense.examsystem.commons.Actions;
-import io.n0sense.examsystem.commons.CommonConstants;
-import io.n0sense.examsystem.commons.CommonStatus;
+import io.n0sense.examsystem.commons.constants.Actions;
+import io.n0sense.examsystem.commons.constants.Identities;
+import io.n0sense.examsystem.commons.constants.Status;
 import io.n0sense.examsystem.entity.Admin;
 import io.n0sense.examsystem.entity.Log;
 import io.n0sense.examsystem.entity.R;
@@ -23,7 +23,9 @@ import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 管理员页面中的REST API控制器。
@@ -37,24 +39,38 @@ public class AdminRestController {
     private final Logger logger = LoggerFactory.getLogger(AdminRestController.class);
 
     @PostMapping({"/register"})
+    @ResponseBody
     @RecordLog(action = Actions.LOGIN)
-    public ModelAndView register(String username, String password, String groupName, HttpServletRequest request, Model model) {
+    public R register(String username, String password, String groupName, HttpServletRequest request) {
         int result = this.adminService.register(username, password, groupName);
-        if (result == CommonStatus.OK) {
-            // 检查判断为登陆成功
+        Map<String, Object> data = new HashMap<>();
+        if (result == Status.OK) {
+            // 检查判断为注册成功
             request.getSession().setAttribute("username", username);
             request.getSession().setAttribute("group", groupName);
-            request.getSession().setAttribute("role", CommonConstants.ROLE_ADMIN);
+            request.getSession().setAttribute("role", Identities.ROLE_ADMIN);
 
-            return new ModelAndView("/" + CommonConstants.ROLE_ADMIN + "/home");
-        } else if (result == CommonStatus.ERR_USERNAME_IN_USE) {
+            data.put("location", "/" + Identities.ROLE_ADMIN + "/home");
+            return R.builder()
+                    .status(result)
+                    .message("注册成功。")
+                    .data(data)
+                    .build();
+        } else if (result == Status.ERR_USERNAME_IN_USE) {
             // 用户密码已经存在
-            model.addAttribute("msg", "用户名已占用。");
-            return new ModelAndView("/" + CommonConstants.ROLE_ADMIN + "/register");
+            return R.builder()
+                    .status(result)
+                    .message("用户名已占用。")
+                    .build();
         } else {
             // 其他错误
             this.logger.error("register: Unresolved result " + result);
-            return new ModelAndView("404");
+            data.put("location", "/404");
+            return R.builder()
+                    .status(Status.ERR_UNSPECIFIED)
+                    .message("register: Unresolved result " + result)
+                    .data(data)
+                    .build();
         }
     }
 
@@ -62,13 +78,13 @@ public class AdminRestController {
     @ResponseBody
     public R register(String username, String password, String groupName) {
         int result = this.adminService.register(username, password, groupName);
-        if (result == CommonStatus.OK) {
+        if (result == Status.OK) {
             // 检查判断为注册成功
             return R.builder()
                     .status(result)
                     .message("用户注册成功。")
                     .build();
-        } else if (result == CommonStatus.ERR_USERNAME_IN_USE) {
+        } else if (result == Status.ERR_USERNAME_IN_USE) {
             // 用户密码已经存在
             return R.builder()
                     .status(result)
@@ -78,7 +94,7 @@ public class AdminRestController {
             // 其他错误
             this.logger.error("register: Unresolved result " + result);
             return R.builder()
-                    .status(CommonStatus.ERR_UNSPECIFIED)
+                    .status(Status.ERR_UNSPECIFIED)
                     .message("发生未知错误。")
                     .build();
         }
@@ -86,47 +102,56 @@ public class AdminRestController {
 
     @PostMapping({"/login"})
     @RecordLog(action = Actions.LOGIN)
-    public ModelAndView login(String username, String password, Model model, HttpServletRequest request) {
+    @ResponseBody
+    public R login(String username, String password, HttpServletRequest request) {
+        Map<String, Object> data = new HashMap<>();
         int result = this.adminService.login(username, password);
         // 通过login方法的话，admin一定不是null，不用担心这里的isPresent()检查
-        Admin admin = this.adminService.findByName(username).get();
+        Admin admin = this.adminService.findByName(username).orElse(new Admin());
         if (result == 0) {
             // 判断为信息正确
             request.getSession().setAttribute("username", username);
             request.getSession().setAttribute("group", admin.getGroupName());
-            request.getSession().setAttribute("role", CommonConstants.ROLE_ADMIN);
+            request.getSession().setAttribute("role", Identities.ROLE_ADMIN);
 
-            return new ModelAndView("/" + CommonConstants.ROLE_ADMIN + "/home");
-        } else if (result == CommonStatus.ERR_INCORRECT_PASSWORD) {
-            // 判断为密码错误
-            model.addAttribute("msg", "密码错误。");
-            return new ModelAndView("/" + CommonConstants.ROLE_ADMIN + "/login");
-        } else if (result == CommonStatus.ERR_USER_NOT_FOUND) {
-            // 判断为用户不存在
-            model.addAttribute("msg", "没有这个用户。");
-            return new ModelAndView("/" + CommonConstants.ROLE_ADMIN + "/login");
+            data.put("location", "/" + Identities.ROLE_ADMIN + "/home");
+            return R.builder()
+                    .status(result)
+                    .message("登陆成功。")
+                    .data(data)
+                    .build();
+        } else if (result == Status.ERR_INCORRECT_PASSWORD
+                || result == Status.ERR_USER_NOT_FOUND) {
+            // 判断为密码错误或用户不存在，但是只能提示密码错误
+            return R.builder()
+                    .status(Status.ERR_INCORRECT_PASSWORD)
+                    .message("密码错误。")
+                    .build();
         } else {
             // 其他错误
             this.logger.error("login: Unresolved result " + result);
-            return new ModelAndView("404");
+            return R.builder()
+                    .status(Status.ERR_UNSPECIFIED)
+                    .message("发生未知错误。")
+                    .build();
         }
     }
 
     @PostMapping({"/publish"})
     public ModelAndView publishEnrollmentInfo(String majorName, int applicant, int enrollment, int admission, double score, String examName, LocalDateTime start, LocalDateTime end, Model model) {
         int result = this.adminService.addEnrollmentInfo(majorName, applicant, enrollment, admission, score, examName, start, end);
-        if (result == CommonStatus.ERR_MAJOR_EXISTS) {
+        if (result == Status.ERR_MAJOR_EXISTS) {
             model.addAttribute("msg", "已经有这个专业了。");
-            return new ModelAndView("/" + CommonConstants.ROLE_ADMIN +
-                    "/" + CommonConstants.GROUP_RECRUIT_AFFAIRS + "/recruit-maintain");
-        } else if (result == CommonStatus.ERR_EXAM_EXISTS) {
+            return new ModelAndView("/" + Identities.ROLE_ADMIN +
+                    "/" + Identities.GROUP_RECRUIT_AFFAIRS + "/recruit-maintain");
+        } else if (result == Status.ERR_EXAM_EXISTS) {
             model.addAttribute("msg", "已经有这项考试了。");
-            return new ModelAndView("/" + CommonConstants.ROLE_ADMIN +
-                    "/" + CommonConstants.GROUP_RECRUIT_AFFAIRS + "/recruit-maintain");
-        } else if (result == CommonStatus.OK) {
+            return new ModelAndView("/" + Identities.ROLE_ADMIN +
+                    "/" + Identities.GROUP_RECRUIT_AFFAIRS + "/recruit-maintain");
+        } else if (result == Status.OK) {
             model.addAttribute("msg", "添加完成。");
-            return new ModelAndView("/" + CommonConstants.ROLE_ADMIN +
-                    "/" + CommonConstants.GROUP_RECRUIT_AFFAIRS + "/recruit-maintain");
+            return new ModelAndView("/" + Identities.ROLE_ADMIN +
+                    "/" + Identities.GROUP_RECRUIT_AFFAIRS + "/recruit-maintain");
         } else {
             this.logger.error("publishEnrollmentInfo: Unresolved result " + result);
             return new ModelAndView("404");
@@ -150,18 +175,17 @@ public class AdminRestController {
         return new ModelAndView("/admin/login-history");
     }
 
-    // TODO: 这不是真的REST，得改
     @PostMapping("/resetPassword")
     @ResponseBody
     public R resetPassword(Long id) {
         if (adminService.resetPassword(id)) {
             return R.builder()
-                    .status(CommonStatus.OK)
+                    .status(Status.OK)
                     .message("ID为" + id + "的用户的密码重置成功，新密码是1234。")
                     .build();
         } else {
             return R.builder()
-                    .status(CommonStatus.ERR_OPERATION_FAILED)
+                    .status(Status.ERR_OPERATION_FAILED)
                     .message("未能重置ID为" + id + "的用户的密码。")
                     .build();
         }
@@ -172,7 +196,7 @@ public class AdminRestController {
     public R dropUser(Long id) {
         adminService.dropUser(id);
         return R.builder()
-                .status(CommonStatus.OK)
+                .status(Status.OK)
                 .message("ID为" + id + "的用户已经移除。")
                 .build();
     }
