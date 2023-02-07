23233ccb8e6a83ad27ba72df388cd2d3b663b2ac
n0sense <1436648676@qq.com>
2023-02-07 16:48:52 +0800

    添加用于标记方法不应当有null参数的注解

---
+++ src/main/java/io/n0sense/examsystem/annotation/NoNullArgs.java
@@ -0,0 +1,17 @@
+package io.n0sense.examsystem.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * 只能为方法添加该注解，且返回类型应该是{@link io.n0sense.examsystem.entity.R R}。<br>
+ * 如果为一个方法添加这个注解，那么配套的AOP增强类将会在该方法被调用时检查实参列表中是否包含null对象。一旦存在，返回类型
+ * {@link io.n0sense.examsystem.entity.R R}的返回代码将会修改为1001（参见{@link io.n0sense.examsystem.commons.constants.Status Status}）
+ * ，且data中将会添加/覆盖属性"location"为400页面地址。
+ */
+@Target(ElementType.METHOD)
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+public @interface NoNullArgs {
+    String[] excludedNames() default {};
+    Class[] excludedTypes() default {};
+}
