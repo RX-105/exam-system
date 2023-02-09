f826020bf4f95bc3002046981ffdb7de2213b69a
n0sense <1436648676@qq.com>
2023-02-09 17:28:55 +0800

    为findAll方法添加分页支持

--- src/main/java/io/n0sense/examsystem/service/IBackupService.java
+++ src/main/java/io/n0sense/examsystem/service/IBackupService.java
@@ -1,14 +1,14 @@
 package io.n0sense.examsystem.service;
 
 import io.n0sense.examsystem.entity.Backup;
+import org.springframework.data.domain.Page;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.NoSuchElementException;
 
 public interface IBackupService {
     void dumpDatabase() throws IOException;
     void applyFromId(Long id) throws NoSuchElementException, IOException;
     void removeBackup(Long id) throws NoSuchElementException, IOException;
-    List<Backup> findAll();
+    Page<Backup> findAll(int page, int size);
 }
