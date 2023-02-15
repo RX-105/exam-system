165388d9dca4362b040b31dab99fda99b51a6b80
n0sense <1436648676@qq.com>
2023-02-15 17:59:00 +0800

    添加@Builder注解，添加阶段定义者的字段

--- src/main/java/io/n0sense/examsystem/entity/Stage.java
+++ src/main/java/io/n0sense/examsystem/entity/Stage.java
@@ -14,6 +14,7 @@ import java.util.Objects;
 @ToString
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 @Table(name = "stage")
 public class Stage {
     @Id
@@ -27,6 +28,7 @@ public class Stage {
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm")
     private LocalDateTime endTime;
     private String remark;
+    private String definer;
 
     @Override
     public boolean equals(Object o) {
