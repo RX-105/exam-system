86c1d61c049d4c1d5c497251fe184a7f6f3723d3
n0sense <1436648676@qq.com>
2023-02-15 17:57:54 +0800

    添加Stage实体的仓库接口

--- src/main/java/io/n0sense/examsystem/repository/StageRepository.java
+++ src/main/java/io/n0sense/examsystem/repository/StageRepository.java
@@ -3,5 +3,23 @@ package io.n0sense.examsystem.repository;
 import io.n0sense.examsystem.entity.Stage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+
 public interface StageRepository extends JpaRepository<Stage, Long> {
+    /**
+     * 检查数据库内是否存在名称相同、时间存在交集且ID不同的记录。
+     * @param name 阶段名称。
+     * @param start 阶段开始时间。
+     * @param end 阶段结束时间。
+     * @param stageId 阶段ID。用于更新某一个记录时，ID使用原纪录的ID；新增记录时，使用小于100000的任何ID。
+     * @return 一个布尔值，用于判断数据库内确实存在满足条件的记录。
+     */
+//    @Query("""
+//            select (count(s) > 0) from Stage s
+//            where s.name = ?1
+//            and s.endTime > ?2
+//            and s.startTime < ?3
+//            """)
+    boolean existsByNameAndEndTimeAfterAndStartTimeBeforeAndStageIdNot
+    (String name, LocalDateTime start, LocalDateTime end, Long stageId);
 }
