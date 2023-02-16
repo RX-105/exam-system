f8e2f451d4519d8d504586f123d9760f5db1185e
n0sense <1436648676@qq.com>
2023-02-16 22:38:49 +0800

    把所有常量的类型改成Map.Set<String, String>，加入常量对应中文

--- src/main/java/io/n0sense/examsystem/commons/constants/Identities.java
+++ src/main/java/io/n0sense/examsystem/commons/constants/Identities.java
@@ -1,10 +1,18 @@
 package io.n0sense.examsystem.commons.constants;
 
-public interface Identities {
-    String ROLE_STUDENT = "student";
-    String ROLE_ADMIN = "admin";
+import java.util.AbstractMap;
+import java.util.Map;
 
-    String GROUP_SYSTEM_ADMIN = "sudoers";
-    String GROUP_ACADEMIC_AFFAIRS = "aca-aff";
-    String GROUP_RECRUIT_AFFAIRS = "recruit";
+public interface Identities {
+    Map.Entry<String, String> ROLE_STUDENT =
+            new AbstractMap.SimpleEntry<>("student", "学生");
+    Map.Entry<String, String> ROLE_ADMIN =
+            new AbstractMap.SimpleEntry<>("admin", "管理员");
+
+    Map.Entry<String, String> GROUP_SYSTEM_ADMIN =
+            new AbstractMap.SimpleEntry<>("sudoers", "网站管理员");
+    Map.Entry<String, String> GROUP_ACADEMIC_AFFAIRS =
+            new AbstractMap.SimpleEntry<>("aca-aff", "教务管理员");
+    Map.Entry<String, String> GROUP_RECRUIT_AFFAIRS =
+            new AbstractMap.SimpleEntry<>("recruit", "招生管理员");
 }
