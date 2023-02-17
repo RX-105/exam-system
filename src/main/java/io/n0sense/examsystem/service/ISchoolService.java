788e2f4ec2e6bd56ce67ba876f5ed49736768787
n0sense <1436648676@qq.com>
2023-02-17 11:28:22 +0800

    创建SchoolService接口以及实现

---
+++ src/main/java/io/n0sense/examsystem/service/ISchoolService.java
@@ -0,0 +1,9 @@
+package io.n0sense.examsystem.service;
+
+import io.n0sense.examsystem.entity.School;
+
+import java.util.List;
+
+public interface ISchoolService {
+    List<School> findAll();
+}
---
+++ src/main/java/io/n0sense/examsystem/service/impl/SchoolService.java
@@ -0,0 +1,20 @@
+package io.n0sense.examsystem.service.impl;
+
+import io.n0sense.examsystem.entity.School;
+import io.n0sense.examsystem.repository.SchoolRepository;
+import io.n0sense.examsystem.service.ISchoolService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class SchoolService implements ISchoolService {
+    private final SchoolRepository schoolRepository;
+
+    @Override
+    public List<School> findAll() {
+        return schoolRepository.findAll();
+    }
+}
