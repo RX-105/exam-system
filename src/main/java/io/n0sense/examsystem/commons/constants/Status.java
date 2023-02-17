0acfcfb3fa0f5070cee2b8f08df5623b702e67f3
n0sense <1436648676@qq.com>
2023-02-17 12:33:51 +0800

    添加参数不匹配的状态码

--- src/main/java/io/n0sense/examsystem/commons/constants/Status.java
+++ src/main/java/io/n0sense/examsystem/commons/constants/Status.java
@@ -8,11 +8,14 @@ package io.n0sense.examsystem.commons.constants;
 public interface Status {
     int ERR_UNSPECIFIED = -1;
     int OK = 0;
+
     // 通用错误状态
     int ERR_OPERATION_FAILED = 100;
     int ERR_PARAMETER_NOT_PRESENT = 101;
+    int ERR_PARAMETER_NOT_MATCH = 102;
     int ERR_NO_SUCH_ELEMENT = 102;
     int ERR_FILE_NOT_FOUND = 103;
+
     // 详细错误状态
     int ERR_USERNAME_IN_USE = 1001;
     int ERR_INCORRECT_PASSWORD = 1002;
