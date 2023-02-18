ad78230933b0ebde0c8d191da0115a8f367faaab
n0sense <1436648676@qq.com>
2023-02-18 22:55:18 +0800

    新增一个简单的登陆控制拦截器

---
+++ src/main/java/io/n0sense/examsystem/interceptor/SecurityInterceptor.java
@@ -0,0 +1,29 @@
+package io.n0sense.examsystem.interceptor;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.java.Log;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Component
+@Log
+public class SecurityInterceptor implements HandlerInterceptor {
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        String path = request.getServletPath();
+        // 检查登陆状态
+        String username = (String) request.getSession().getAttribute("username");
+        if (path.contains("error") || path.contains("index") || path.contains("login")
+                || path.contains("register") || path.equals("/")) {
+            return true;
+        } else {
+            if (username == null) {
+                request.getRequestDispatcher("/index").forward(request, response);
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+}
