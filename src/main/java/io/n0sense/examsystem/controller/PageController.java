660c1272272e0bd78761547911bfd2018f646b3f
n0sense <1436648676@qq.com>
2023-02-18 22:54:35 +0800

    映射index页面

--- src/main/java/io/n0sense/examsystem/controller/PageController.java
+++ src/main/java/io/n0sense/examsystem/controller/PageController.java
@@ -1,12 +1,22 @@
 package io.n0sense.examsystem.controller;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class PageController {
+    @Value("${application.version}")
+    private String version;
+
+    @ModelAttribute
+    public void addCommonAttributes(Model model) {
+        model.addAttribute("version", version);
+    }
     // 不应该使用通配符把所有URL都拦截到这里来，因为这样会误杀静态资源
     // 只能把用到的页面映射一下
     @GetMapping("/sessionAttr")
@@ -17,5 +27,10 @@ public class PageController {
     @GetMapping({"/admin/404", "/student/404"})
     public String get404View(){
         return "/404";
+    }
+
+    @GetMapping("/index")
+    public String getIndexView() {
+        return "/index";
     }
 }
