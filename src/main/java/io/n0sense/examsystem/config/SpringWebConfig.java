ae12bcab676ab189eb77d686dde0d41d0e8c3965
n0sense <1436648676@qq.com>
2023-02-18 22:55:41 +0800

    配置登陆控制拦截器SecurityInterceptor

--- src/main/java/io/n0sense/examsystem/config/SpringWebConfig.java
+++ src/main/java/io/n0sense/examsystem/config/SpringWebConfig.java
@@ -1,5 +1,6 @@
 package io.n0sense.examsystem.config;
 
+import io.n0sense.examsystem.interceptor.SecurityInterceptor;
 import io.n0sense.examsystem.interceptor.StatisticsInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -9,13 +10,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class SpringWebConfig implements WebMvcConfigurer {
     private final StatisticsInterceptor statisticsInterceptor;
+    private final SecurityInterceptor securityInterceptor;
     @Autowired
-    public SpringWebConfig(StatisticsInterceptor statisticsInterceptor){
+    public SpringWebConfig(StatisticsInterceptor statisticsInterceptor, SecurityInterceptor securityInterceptor){
         this.statisticsInterceptor = statisticsInterceptor;
+        this.securityInterceptor = securityInterceptor;
     }
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(statisticsInterceptor)
+                .addPathPatterns("/**")
+                .excludePathPatterns("/static/**", "/api/**");
+        registry.addInterceptor(securityInterceptor)
                 .addPathPatterns("/**")
                 .excludePathPatterns("/static/**", "/api/**");
     }
