632ba7f2ac92fffb2bcd3fc27ec93d06037d1db0
n0sense <1436648676@qq.com>
2023-03-01 22:02:50 +0800

    为注册方法添加@NotNull注解

--- src/main/java/io/n0sense/examsystem/service/IUserService.java
+++ src/main/java/io/n0sense/examsystem/service/IUserService.java
@@ -1,11 +1,13 @@
 package io.n0sense.examsystem.service;
 
 import io.n0sense.examsystem.entity.User;
+import lombok.NonNull;
 
 import java.util.Optional;
 
 public interface IUserService {
-    int register(String username, String password, String ip);
+    int register(@NonNull String username, @NonNull String password);
+
     int login(String username, String password);
     Optional<User> findByName(String username);
 }
