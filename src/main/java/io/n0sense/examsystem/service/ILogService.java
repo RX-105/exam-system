f37fe9ccaf2b5c6f84710d0bed7a5c02f5e72a92
n0sense <1436648676@qq.com>
2023-03-05 17:49:16 +0800

    添加record方法

--- src/main/java/io/n0sense/examsystem/service/ILogService.java
+++ src/main/java/io/n0sense/examsystem/service/ILogService.java
@@ -1,11 +1,14 @@
 package io.n0sense.examsystem.service;
 
 import io.n0sense.examsystem.entity.Log;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.data.domain.Page;
 
 import java.time.LocalDate;
 
 public interface ILogService {
+    void record(String action, String message, HttpServletRequest request);
+
     Page<Log> getUserLogins(String username, int page, int size);
     Page<Log> getGroupLogin(String groupName, int page, int size);
     Page<Log> queryLogByTimeRange(String username, int page, int size,
