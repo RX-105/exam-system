27106719ed3027163077ba292a67c2ed577c9d90
n0sense <1436648676@qq.com>
2023-03-05 17:52:59 +0800

    添加按名称和学校ID查找学校方法和按学校ID查找所有学校方法

--- src/main/java/io/n0sense/examsystem/repository/MajorRepository.java
+++ src/main/java/io/n0sense/examsystem/repository/MajorRepository.java
@@ -3,5 +3,9 @@ package io.n0sense.examsystem.repository;
 import io.n0sense.examsystem.entity.Major;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MajorRepository extends JpaRepository<Major, Long>, BaseRepository<Major> {
+    boolean existsByNameAndSchoolId(String name, Long schoolId);
+    List<Major> findAllBySchoolId(Long schoolId);
 }
