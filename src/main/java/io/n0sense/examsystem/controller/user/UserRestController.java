1c0f3a15b91a744213ac965344f5355160eb5fa2
n0sense <1436648676@qq.com>
2023-03-05 22:32:42 +0800

    添加文件上传和下载API

--- src/main/java/io/n0sense/examsystem/controller/user/UserRestController.java
+++ src/main/java/io/n0sense/examsystem/controller/user/UserRestController.java
@@ -9,18 +9,22 @@ import io.n0sense.examsystem.entity.Major;
 import io.n0sense.examsystem.entity.R;
 import io.n0sense.examsystem.entity.Stage;
 import io.n0sense.examsystem.entity.User;
-import io.n0sense.examsystem.service.impl.MajorService;
-import io.n0sense.examsystem.service.impl.StageService;
-import io.n0sense.examsystem.service.impl.UserService;
+import io.n0sense.examsystem.service.impl.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.YearMonth;
 import java.util.*;
@@ -35,6 +39,8 @@ public class UserRestController {
     private final UserService userService;
     private final MajorService majorService;
     private final StageService stageService;
+    private final FileService fileService;
+    private final LogService logService;
 
     @ExceptionHandler(NullPointerException.class)
     public R nullParameterHandler(NullPointerException e) {
@@ -256,5 +262,80 @@ public class UserRestController {
                 .status(Status.OK)
                 .data(Map.of("user-info", userService.findById(id).orElseThrow()))
                 .build();
+    }
+
+    @PostMapping("/avatar")
+    public R setAvatar(MultipartFile file) {
+        String filename;
+        try {
+            filename = fileService.saveAvatar(file);
+        } catch (IOException e) {
+            return R.builder()
+                    .status(Status.ERR_OPERATION_FAILED)
+                    .message("文件保存失败。")
+                    .build();
+        }
+        List<Stage> stages;
+        Long uid = (Long) request.getSession().getAttribute("uid");
+        if (uid == null) {
+            return R.builder()
+                    .status(Status.ERR_LOGIN_REQUIRED)
+                    .message("需要登陆。")
+                    .build();
+        }
+        Optional<User> opUser = userService.findById(uid);
+        if (opUser.isEmpty()) {
+            return R.builder()
+                    .status(Status.ERR_NO_SUCH_ELEMENT)
+                    .build();
+        }
+        User user = opUser.get();
+        // 获取学校ID，判断是否在报名期间内
+        if (user.getSchoolId() != null) {
+            stages = stageService.findAllStageBySchoolIdAndName(
+                    user.getSchoolId(), Stages.REGISTER.getKey());
+        } else {
+            return R.builder()
+                    .status(Status.ERR_PARAMETER_NOT_PRESENT)
+                    .message("请先填写报考学校。")
+                    .data(Map.of("param", "schoolId"))
+                    .build();
+        }
+        if (stages.size() == 0) {
+            return R.builder()
+                    .status(Status.ERR_TIME_NOT_FOUND)
+                    .message("目标报考学校没有设置在线报名时间，不能修改信息。请联系报考学校。")
+                    .build();
+        } else {
+            user.setAvatarName(filename);
+            userService.save(user);
+            logService.record(Actions.UPDATE_AVATAR, filename, request);
+            return R.builder()
+                    .status(Status.OK)
+                    .message("个人照片修改成功")
+                    .data(Map.of("filename", filename))
+                    .build();
+        }
+    }
+
+    @PostMapping("/avatar/{filename}")
+    public ResponseEntity<Object> getAvatar(@PathVariable("filename") String filename) {
+        Resource fileResource;
+        try {
+            fileResource = fileService.getAvatar(filename);
+            HttpHeaders headers = new HttpHeaders();
+            headers.add("Content-Disposition", String.format("attachment;filename=\"%s", filename));
+            headers.add("Cache-Control", "no-cache,no-store,must-revalidate");
+            headers.add("Pragma", "no-cache");
+            headers.add("Expires", "0");
+
+            return ResponseEntity.ok()
+                    .headers(headers)
+                    .contentLength(fileResource.contentLength())
+                    .contentType(MediaType.IMAGE_PNG)
+                    .body(fileResource);
+        } catch (Exception e) {
+            return null;
+        }
     }
 }
