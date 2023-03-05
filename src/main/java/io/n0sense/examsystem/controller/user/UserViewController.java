e68cf9ae2080648860f9a9db012678ad72a8ea8d
n0sense <1436648676@qq.com>
2023-03-05 18:00:29 +0800

    实现用户登录、学生注册和考试注册的页面

--- src/main/java/io/n0sense/examsystem/controller/user/UserViewController.java
+++ src/main/java/io/n0sense/examsystem/controller/user/UserViewController.java
@@ -1,5 +1,12 @@
 package io.n0sense.examsystem.controller.user;
 
+import io.n0sense.examsystem.commons.constants.Stages;
+import io.n0sense.examsystem.entity.Log;
+import io.n0sense.examsystem.service.impl.LogService;
+import io.n0sense.examsystem.service.impl.SchoolService;
+import io.n0sense.examsystem.service.impl.StageService;
+import io.n0sense.examsystem.service.impl.UserService;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,16 +17,25 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/student")
 @RequiredArgsConstructor
 public class UserViewController {
     @Value("${application.version}")
     private String version;
+    private final HttpServletRequest request;
+    private final HttpSession session;
+    private final LogService logService;
+    private final SchoolService schoolService;
+    private final StageService stageService;
+    private final UserService userService;
 
     @ModelAttribute
     public void addCommonAttributes(Model model) {
         model.addAttribute("version", version);
+        model.addAttribute("schools", schoolService.findAll());
     }
 
     @GetMapping("/login")
@@ -35,5 +51,42 @@ public class UserViewController {
         } else {
             return new ModelAndView("/student/login");
         }
+    }
+
+    @GetMapping("/login-history")
+    public ModelAndView getStudentLoginHistoryView(Integer page, Model model) {
+        var loginHistoryPage = logService.getUserLogins(
+                (String) request.getSession().getAttribute("username"),
+                (null == page ? 0 : page),
+                10
+        );
+        List<Log> loginHistory = loginHistoryPage.toList();
+
+        if (page != null && loginHistoryPage.getTotalPages() < page){
+            model.addAttribute("msg", "请勿玩弄页面参数哦。");
+        } else {
+            model.addAttribute("loginsPage", loginHistoryPage);
+            model.addAttribute("logins", loginHistory);
+        }
+
+        return new ModelAndView("/student/login-history");
+    }
+
+    @GetMapping("/registration-notice")
+    public ModelAndView getStudentRegistrationNoticeView(Long schoolId, Model model) {
+        if (schoolId != null) {
+            String schoolName = schoolService.findSchool(schoolId).getName();
+            model.addAttribute("stages", stageService.findAllStageBySchoolId(schoolId));
+            model.addAttribute("schoolName", schoolName);
+            model.addAttribute("stageMap", Stages.stages);
+        }
+        return new ModelAndView("/student/registration-notice");
+    }
+
+    @GetMapping("/exam-register")
+    public ModelAndView getStudentExamRegisterView(Model model) {
+        model.addAttribute("user",
+                userService.findById((Long) session.getAttribute("uid")).orElseThrow());
+        return new ModelAndView("/student/exam-register");
     }
 }
