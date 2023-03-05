051e05cc93d310789f29e232d75bee0184a3cfb3
n0sense <1436648676@qq.com>
2023-03-05 17:58:17 +0800

    实现学生用户注册页面

--- src/main/java/io/n0sense/examsystem/controller/user/UserPageController.java
+++ src/main/java/io/n0sense/examsystem/controller/user/UserPageController.java
@@ -1,5 +1,7 @@
 package io.n0sense.examsystem.controller.user;
 
+import io.n0sense.examsystem.service.impl.SchoolService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -7,17 +9,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 
 @Controller
+@RequiredArgsConstructor
 public class UserPageController {
     @Value("${application.version}")
     private String version;
+    private final SchoolService schoolService;
 
     @ModelAttribute
     public void addCommonAttributes(Model model) {
         model.addAttribute("version", version);
+        model.addAttribute("schools", schoolService.findAll());
     }
 
     @GetMapping("/student/home")
     public String getStudentHomeView() {
         return "/student/home";
+    }
+
+    @GetMapping("/student/register")
+    public String getStudentRegisterView() {
+        return "/student/register";
     }
 }
