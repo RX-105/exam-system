9e885ff0bf8da66e28b27150cc4c5547c7b46e3d
n0sense <1436648676@qq.com>
2023-03-07 21:31:03 +0800

    增加按专业查找考试的功能

--- src/main/java/io/n0sense/examsystem/repository/ExamRepository.java
+++ src/main/java/io/n0sense/examsystem/repository/ExamRepository.java
@@ -1,7 +1,10 @@
 package io.n0sense.examsystem.repository;
 
 import io.n0sense.examsystem.entity.Exam;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ExamRepository extends JpaRepository<Exam, Long>, BaseRepository<Exam> {
+    Page<Exam> findAllByMajorId(Long majorId, Pageable pageable);
 }
