7f33949acab3be0396c8f14d3f1e5f559d2bf4e7
n0sense <1436648676@qq.com>
2023-03-08 15:15:18 +0800

    添加并实现按id查找学校的方法

--- src/main/java/io/n0sense/examsystem/service/ISchoolService.java
+++ src/main/java/io/n0sense/examsystem/service/ISchoolService.java
@@ -3,8 +3,9 @@ package io.n0sense.examsystem.service;
 import io.n0sense.examsystem.entity.School;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface ISchoolService {
     List<School> findAll();
-    School findSchool(Long schoolId);
+    Optional<School> findSchool(Long schoolId);
 }
