bb6c58f2cb68d19a2fe7c4711fb08af05b3277e4
n0sense <1436648676@qq.com>
2023-03-08 15:14:00 +0800

    修改适配StageRepository的方法名百年更

--- src/test/java/io/n0sense/examsystem/repository/TestStageRepository.java
+++ src/test/java/io/n0sense/examsystem/repository/TestStageRepository.java
@@ -19,25 +19,25 @@ public class TestStageRepository {
         String name = "招考信息发布";
         LocalDateTime start = LocalDateTime.parse("2023-02-14T12:00:00");
         LocalDateTime end = LocalDateTime.parse("2023-03-01T12:00:00");
-        boolean result = stageRepository.existsByNameAndEndTimeAfterAndStartTimeBeforeAndStageIdNot(name, start, end, 100000L);
+        boolean result = stageRepository.existsByNameAndSchoolIdAndEndTimeAfterAndStartTimeBeforeAndStageIdNot(name, 100001L, start, end, 100000L);
         System.out.println(result);
         assert result;
         // TEST 2
         start = LocalDateTime.parse("2023-02-10T12:00:00");
         end = LocalDateTime.parse("2023-02-24T12:00:00");
-        result = stageRepository.existsByNameAndEndTimeAfterAndStartTimeBeforeAndStageIdNot(name, start, end, 100000L);
+        result = stageRepository.existsByNameAndSchoolIdAndEndTimeAfterAndStartTimeBeforeAndStageIdNot(name, 100001L, start, end, 100000L);
         System.out.println(result);
         assert result;
         // TEST 3
         start = LocalDateTime.parse("2023-02-14T12:00:00");
         end = LocalDateTime.parse("2023-02-22T12:00:00");
-        result = stageRepository.existsByNameAndEndTimeAfterAndStartTimeBeforeAndStageIdNot(name, start, end, 100000L);
+        result = stageRepository.existsByNameAndSchoolIdAndEndTimeAfterAndStartTimeBeforeAndStageIdNot(name, 100001L, start, end, 100000L);
         System.out.println(result);
         assert result;
         // TEST 4
         start = LocalDateTime.parse("2023-02-10T12:00:00");
         end = LocalDateTime.parse("2023-02-11T12:00:00");
-        result = stageRepository.existsByNameAndEndTimeAfterAndStartTimeBeforeAndStageIdNot(name, start, end, 100000L);
+        result = stageRepository.existsByNameAndSchoolIdAndEndTimeAfterAndStartTimeBeforeAndStageIdNot(name, 100001L, start, end, 100000L);
         System.out.println(result);
         assert !result;
         System.out.println(version);
