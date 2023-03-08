0fd046a48ec6a95cc6589f4fd97d32dcf8700288
n0sense <1436648676@qq.com>
2023-03-08 15:13:25 +0800

    添加调试时配置信息

---
+++ src/main/java/io/n0sense/examsystem/config/properties/DebugProperties.java
@@ -0,0 +1,13 @@
+package io.n0sense.examsystem.config.properties;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "application.debug")
+public class DebugProperties {
+    /**
+     * 是否验证操作符合阶段设置，置为true则为需要检查，false表示不检查。默认值为true。
+     */
+    private Boolean validateStageTime = true;
+}
