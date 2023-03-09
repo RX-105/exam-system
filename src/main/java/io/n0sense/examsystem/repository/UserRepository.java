827b0deb43a280a223044b46fab49110cbe0e540
n0sense <1436648676@qq.com>
2023-03-09 21:01:17 +0800

    添加按照学校id查找用户的方法

--- src/main/java/io/n0sense/examsystem/repository/UserRepository.java
+++ src/main/java/io/n0sense/examsystem/repository/UserRepository.java
@@ -3,5 +3,8 @@ package io.n0sense.examsystem.repository;
 import io.n0sense.examsystem.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long>, BaseRepository<User> {
+    List<User> findAllBySchoolId(Long schoolId);
 }
