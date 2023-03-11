bfcc67f569ef8c9fe5ff2816072bf4304f95efd3
n0sense <1436648676@qq.com>
2023-03-11 23:40:53 +0800

    添加按照学校ID和是否确认搜索、查询计数方法和更新考场信息方法

--- src/main/java/io/n0sense/examsystem/repository/UserRepository.java
+++ src/main/java/io/n0sense/examsystem/repository/UserRepository.java
@@ -1,10 +1,30 @@
 package io.n0sense.examsystem.repository;
 
+import io.n0sense.examsystem.dto.BasicUserDTO;
+import io.n0sense.examsystem.dto.ExamUserDTO;
+import io.n0sense.examsystem.entity.School;
 import io.n0sense.examsystem.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface UserRepository extends JpaRepository<User, Long>, BaseRepository<User> {
-    List<User> findAllBySchoolId(Long schoolId);
+    List<BasicUserDTO> findAllBySchool(School school);
+
+    List<BasicUserDTO> findBySchoolAndIsConfirmedTrue(School school);
+    Page<ExamUserDTO> findAllBySchoolIs(School school, Pageable pageable);
+
+    long countBySchool(School school);
+
+    long countBySchoolAndIsConfirmedTrue(School school);
+
+    @Transactional
+    @Modifying
+    @Query("update User u set u.seatId = ?1, u.roomId = ?2 where u.userId = ?3")
+    void updateSeatAndRoom(Long seatId, Long roomId, Long userId);
 }
