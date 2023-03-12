5cf0860725416e462ebc7a3df72f6046d79fc3f1
n0sense <1436648676@qq.com>
2023-03-12 22:48:59 +0800

    添加BufferedImage对象的HttpMessageConvertor

--- src/main/java/io/n0sense/examsystem/config/CommonConfiguration.java
+++ src/main/java/io/n0sense/examsystem/config/CommonConfiguration.java
@@ -5,9 +5,12 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
+import org.springframework.http.converter.BufferedImageHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.handler.SimpleUrlHandlerMapping;
 import org.springframework.web.servlet.resource.ResourceHttpRequestHandler;
 
+import java.awt.image.BufferedImage;
 import java.util.Collections;
 import java.util.List;
 
@@ -32,5 +35,10 @@ public class CommonConfiguration {
         List<Resource> locations = List.of(classPathResource);
         requestHandler.setLocations(locations);
         return requestHandler;
+    }
+
+    @Bean
+    public HttpMessageConverter<BufferedImage> createImageHttpMessageConverter() {
+        return new BufferedImageHttpMessageConverter();
     }
 }
