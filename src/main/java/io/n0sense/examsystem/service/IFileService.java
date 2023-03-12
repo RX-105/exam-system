d3cb748a6c5f8a5228da8d7b1ad2d7bf3016e815
n0sense <1436648676@qq.com>
2023-03-12 22:50:32 +0800

    添加临时文件生成和读写的方法，改用ApplicationProperties配置信息类获取配置信息

--- src/main/java/io/n0sense/examsystem/service/IFileService.java
+++ src/main/java/io/n0sense/examsystem/service/IFileService.java
@@ -3,13 +3,23 @@ package io.n0sense.examsystem.service;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Optional;
 
 public interface IFileService {
     String saveFile(MultipartFile file, String subdir) throws IOException;
     String saveAvatar(MultipartFile file) throws IOException;
+
+    Optional<FileInputStream> createTempFile(String filename) throws IOException;
+
+    Optional<FileOutputStream> saveTempFile(String filename) throws FileNotFoundException;
+
     Resource getFile(String filename, String subdir) throws FileNotFoundException, MalformedURLException;
     Resource getAvatar(String filename) throws FileNotFoundException, MalformedURLException;
+
+    Resource getTempFile(String filename) throws MalformedURLException, FileNotFoundException;
 }
--- src/main/java/io/n0sense/examsystem/service/impl/FileService.java
+++ src/main/java/io/n0sense/examsystem/service/impl/FileService.java
@@ -1,28 +1,26 @@
 package io.n0sense.examsystem.service.impl;
 
+import io.n0sense.examsystem.config.properties.ApplicationProperties;
 import io.n0sense.examsystem.service.IFileService;
 import io.n0sense.examsystem.util.PasswordEncoder;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.java.Log;
 import org.apache.tomcat.util.http.fileupload.InvalidFileNameException;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
+import java.io.*;
 import java.net.MalformedURLException;
+import java.util.Optional;
 
 @Service
 @Log
 @RequiredArgsConstructor
 public class FileService implements IFileService {
-    @Value("${application.app-data-location}")
-    private String appDataPath;
+    private final ApplicationProperties applicationProperties;
 
     @Override
     public String saveFile(MultipartFile file, String subdir) throws IOException {
@@ -34,7 +32,7 @@ public class FileService implements IFileService {
                 + "-"
                 + PasswordEncoder.MD5Encrypt(file.getOriginalFilename()).substring(0,6)
                 + file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
-        String path = appDataPath + subdir + File.separator + filename;
+        String path = applicationProperties.getAppDataLocation() + subdir + File.separator + filename;
         file.transferTo(new File(path));
         return filename;
     }
@@ -44,8 +42,62 @@ public class FileService implements IFileService {
         return saveFile(file, File.separator + "avatars");
     }
 
+    /**
+     * 在程序临时目录下创建一个文件，命名为给定参数。返回一个文件输入流，用于对创建的文件进行IO操作。
+     * @param filename 要创建的文件的文件名。
+     * @return 一个Optional对象。如果文件不存在且成功创建文件，则该对象包含一个文件IO流；
+     * 如果文件已经存在，则返回空Optional对象，而不是删除文件。
+     * @throws IOException 如果无法创建文件，或是出现其他IO异常，将会抛出这个错误。
+     */
+    @Override
+    public Optional<FileInputStream> createTempFile(String filename) throws IOException {
+        File subdir = new File(applicationProperties.getAppDataLocation()
+                + File.separator + "temp");
+        if (!subdir.exists()) {
+            if (!subdir.mkdirs()) {
+                return Optional.empty();
+            }
+        }
+        String path = applicationProperties.getAppDataLocation()
+                + File.separator + "temp" + File.separator + filename;
+        File temp = new File(path);
+        if (!temp.exists()) {
+            if (!temp.createNewFile()) {
+                throw new IOException("无法创建文件。");
+            } else {
+                return Optional.of(new FileInputStream(temp));
+            }
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    /**
+     * 创建给定文件名的文件输出流。
+     * @param filename 文件名
+     * @return 一个Optional对象。如果参数指向一个合法文件，则Optional中包含一个文件输出流，否则为空Optional对象。
+     * @throws FileNotFoundException 表示给定文件名的文件不存在
+     */
+    @Override
+    public Optional<FileOutputStream> saveTempFile(String filename) throws FileNotFoundException {
+        File subdir = new File(applicationProperties.getAppDataLocation() + File.separator + "temp");
+        if (!subdir.exists()) {
+            if (!subdir.mkdirs()) {
+                return Optional.empty();
+            }
+        }
+        String path = applicationProperties.getAppDataLocation()
+                + File.separator + "temp" + File.separator + filename;
+        File file = new File(path);
+        if (!file.exists() || file.isDirectory()) {
+            return Optional.empty();
+        }
+        return Optional.of(new FileOutputStream(file));
+    }
+
     @Override
     public Resource getFile(String filename, String subdir) throws FileNotFoundException, MalformedURLException {
+        String appDataPath = applicationProperties.getAppDataLocation();
         File file = new File(appDataPath + subdir + File.separator + filename);
         if (!file.exists() || file.isDirectory()) {
             log.warning(appDataPath + subdir + File.separator + filename + " not found");
@@ -57,5 +109,10 @@ public class FileService implements IFileService {
     @Override
     public Resource getAvatar(String filename) throws FileNotFoundException, MalformedURLException {
         return getFile(filename, File.separator + "avatars");
+    }
+
+    @Override
+    public Resource getTempFile(String filename) throws MalformedURLException, FileNotFoundException {
+        return getFile(filename, File.separator + "temp");
     }
 }
