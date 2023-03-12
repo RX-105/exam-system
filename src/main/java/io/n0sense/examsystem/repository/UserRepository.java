c24d43339ae3fa7716f1dff3ab03906f210e1299
n0sense <1436648676@qq.com>
2023-03-12 22:46:54 +0800

    修改findAllBySchoolIs方法，添加已确认字段为真的检查

--- src/main/java/io/n0sense/examsystem/repository/UserRepository.java
+++ src/main/java/io/n0sense/examsystem/repository/UserRepository.java
@@ -17,7 +17,7 @@ public interface UserRepository extends JpaRepository<User, Long>, BaseRepositor
     List<BasicUserDTO> findAllBySchool(School school);
 
     List<BasicUserDTO> findBySchoolAndIsConfirmedTrue(School school);
-    Page<ExamUserDTO> findAllBySchoolIs(School school, Pageable pageable);
+    Page<ExamUserDTO> findAllBySchoolAndIsConfirmedTrue(School school, Pageable pageable);
 
     long countBySchool(School school);
 
