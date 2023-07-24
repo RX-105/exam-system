255a27129ce16dd22ff6a66250248f97be15a2bd
n0sense <1436648676@qq.com>
2023-07-24 22:57:47 +0800

    feat: 添加对话框和snackbar的state、getter和展示关闭方法

--- web/src/stores/index.ts
+++ web/src/stores/index.ts
@@ -4,18 +4,67 @@ const store = createStore({
     state () {
         return {
             authStat: false,
-            currUsername: ''
+            currUsername: '',
+            userGroup: '',
+            userRole: '',
+
+            dialogueState: false,
+            dialogueHtml: '',
+
+            snackbarState: false,
+            snackbarText: '',
         }
     },
     mutations: {
         updateAuthState(state, options) {
             state.authStat = options.authStat
             state.currUsername = options.currUsername
+            state.userGroup = options.userGroup
+            state.userRole = options.userRole
+        },
+        showDialogue(state, contentHtml) {
+            state.dialogueState = true
+            state.dialogueHtml = contentHtml
+        },
+        closeDialogue(state) {
+            state.dialogueState = false
+            setTimeout(() => {
+                state.dialogueHtml = ''
+            }, 500)
+        },
+        showSnackbar(state, contentText) {
+            state.snackbarState = true
+            state.snackbarText = contentText
+        },
+        closeSnackbar(state) {
+            state.snackbarState = false
+            setTimeout(() => {
+                state.snackbarText = ''
+            }, 500)
         }
     },
     getters: {
         authStat(state) {
             return state.authStat
+        },
+        userInfo(state) {
+            return {
+                currUsername: state.currUsername,
+                userGroup: state.userGroup,
+                userRole: state.userRole,
+            }
+        },
+        dialogueHtml(state) {
+            return state.dialogueHtml
+        },
+        dialogueState(state) {
+            return state.dialogueState
+        },
+        snackbarText(state) {
+            return state.snackbarText
+        },
+        snackbarState(state) {
+            return state.snackbarState
         }
     }
 })
