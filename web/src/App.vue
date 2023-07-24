7f2cdfa7e5cb6db945b0b9cca1bcd863d86b0d74
n0sense <1436648676@qq.com>
2023-07-24 23:05:44 +0800

    feat: 在全局页面下添加对话框和snackbar

--- web/src/App.vue
+++ web/src/App.vue
@@ -9,11 +9,38 @@
                 <v-btn color="blue" variant="text" @click="mainStore.onHideUp"> Close </v-btn>
             </template>
         </v-snackbar>
+        <!--全局信息提示对话框-->
+        <v-dialog
+            v-model="store.getters.dialogueState"
+            width="auto">
+            <v-card>
+                <v-card-text v-html="store.getters.dialogueHtml"></v-card-text>
+                <v-card-actions>
+                    <v-btn color="primary" block @click="store.commit('closeDialogue')">好</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+        <!--全局信息提示snackbar-->
+        <v-snackbar v-model="store.getters.snackbarState">
+            {{ store.getters.snackbarText }}
+            <template v-slot:actions>
+                <v-btn
+                    color="primary"
+                    variant="text"
+                    @click="store.commit('closeSnackbar')">
+                    关闭
+                </v-btn>
+            </template>
+        </v-snackbar>
     </v-app>
 </template>
 <script setup lang="ts">
 import { RouterView } from 'vue-router';
 import { useMainStore } from '@/stores/appMain';
+import {useStore} from "vuex";
+
 const mainStore = useMainStore();
+const store = useStore()
+
 </script>
 <style scoped lang="scss"></style>
