d1dde9522ecd401a2cec8816ca0a465d58fabb7e
n0sense <1436648676@qq.com>
2023-07-27 17:34:34 +0800

    refactor: 修改导入方法

--- web/src/main.ts
+++ web/src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 import './styles/index.scss';
 import App from './App.vue';
-import router from './router';
+import {router} from "@/router";
 import { vuetify } from '@/plugins/vuetify';
 import '@/plugins/pwa';
 import axios from "axios";
