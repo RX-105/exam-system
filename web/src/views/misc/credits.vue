c920c95ae0552a7534233ebd5878ac7890f718fd
n0sense <1436648676@qq.com>
2023-07-27 17:53:14 +0800

    feat: 添加复制dashboard.vue的两个stub组件

---
+++ web/src/views/misc/credits.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-card class="container">
+        <v-card-title>学校公告</v-card-title>
+        <v-card-subtitle>查看学校最新报考资讯</v-card-subtitle>
+        <v-expansion-panels style="padding: 10px">
+            <v-expansion-panel
+                v-for="stage in registrationInfo.stages">
+                <v-expansion-panel-title>
+                    {{ registrationInfo.stageMap[stage.name] }}
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+<!--                    从{{ stage.startTime }}开始到{{ stage.endTime }}结束，{{ stage.remark }}。-->
+                    开始时间：{{stage.startTime}}<br>
+                    结束时间：{{stage.endTime}}<br>
+                    备注：{{stage.remark}}
+                </v-expansion-panel-text>
+            </v-expansion-panel>
+        </v-expansion-panels>
+    </v-card>
+</template>
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import axios from "axios";
+import {useStore} from "vuex";
+
+const registrationInfo = ref<RegistrationData>({
+    stageMap: {},
+    schoolName: '',
+    stages: [],
+})
+const store = useStore()
+
+onMounted(() => {
+    axios.get("/api/student/registration-notice")
+        .then(res => {
+            if (res.data.status === 0) {
+                registrationInfo.value = res.data.data as RegistrationData
+            }
+        })
+        .catch(err => {
+            store.commit("showDialogue", `加载学校资讯过程中出现了问题。<br/>Error: ${err.message}`)
+        })
+})
+</script>
+<style lang="scss">
+.container {
+    //line-height: 10px;
+}
+</style>
