6cb8d792075c9464e61805567bda804884d8a147
n0sense <1436648676@qq.com>
2023-07-31 23:38:06 +0800

    feat: 添加技术栈介绍的v-card封装组件

---
+++ web/src/views/misc/intro-card.vue
@@ -0,0 +1,39 @@
+<template>
+    <v-card>
+        <v-card-title>{{title}}</v-card-title>
+        <v-card-subtitle>{{subtitle}}</v-card-subtitle>
+        <v-card-text>
+            <v-row>
+                <v-col cols="8">
+                    <slot></slot>
+                </v-col>
+                <v-col cols="4">
+                    <v-img :src="imgSrc"></v-img>
+                </v-col>
+            </v-row>
+        </v-card-text>
+        <v-card-actions>
+            <v-btn color="primary"
+                   @click="openNewTab">
+                查看
+            </v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+    title: String,
+    subtitle: String,
+    imgSrc: String,
+    refSrc: String
+})
+
+function openNewTab() {
+    window.open(props.refSrc, '_blank')
+}
+</script>
+
+<style scoped>
+
+</style>
