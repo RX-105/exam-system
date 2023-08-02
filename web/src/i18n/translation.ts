499f3a29fe8abce6992fa3ed04b6b16767ed9378
n0sense <1436648676@qq.com>
2023-08-02 22:59:44 +0800

    refactor: 配置Dashboard页面的翻译文件

--- web/src/i18n/translation.ts
+++ web/src/i18n/translation.ts
@@ -5,19 +5,23 @@ import app_en from "@/i18n/translations/en/App_en.json";
 import app_zh from "@/i18n/translations/zh/App_zh.json";
 import routes_en from '@/i18n/translations/en/Routes_en.json'
 import routes_zh from '@/i18n/translations/zh/Routes_zh.json'
+import dashboard_en from '@/i18n/translations/en/Dashboard_en.json'
+import dashboard_zh from '@/i18n/translations/zh/Dashboard_zh.json'
 
 const translation = {
     en: {
         $vuetify: en,
         systemLog: systemLog_en,
         app: app_en,
-        routes: routes_en
+        routes: routes_en,
+        dashboard: dashboard_en,
     },
     zh: {
         $vuetify: zhHans,
         systemLog: systemLog_zh,
         app: app_zh,
-        routes: routes_zh
+        routes: routes_zh,
+        dashboard: dashboard_zh,
     }
 }
 
