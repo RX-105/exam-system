1e1b9e05931b19741131e7ac704d6675276ce0f3
n0sense <1436648676@qq.com>
2023-08-15 10:08:19 +0800

    feat: 添加UploadAvatar页面的代码stub

---
+++ web/src/views/student/upload-avatar.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>Hello world.</div>
+</template>
+<script setup lang="ts">
+import {useLocale} from "vuetify";
+
+const { t } = useLocale()
+
+</script>
+<style lang="scss">
+</style>
