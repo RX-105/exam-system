3a93b7109a3ab4af3abfabdab758e99e8ba60399
n0sense <1436648676@qq.com>
2023-08-15 10:01:46 +0800

    feat: 添加R类型和用户信息获取返回类型的定义

--- web/typings/api-response.d.ts
+++ web/typings/api-response.d.ts
@@ -1,4 +1,9 @@
-
+// response entity
+type R = {
+    status: number;
+    message: string;
+    data: { [key: string]: any };
+}
 // /api/student/registration-info
 type RegistrationStageMap = {
     [key: string]: string;
@@ -37,4 +42,57 @@ type LogContent = {
 type LogData = {
     pageInfo: PageInfo;
     content: LogContent[];
+};
+
+// /api/student/user/this
+type School = {
+    schoolId: number;
+    code: string;
+    name: string;
+    address: string;
+    zipcode: number;
+    contactNumber: string;
+    examName: string;
+    recruitYears: string;
+}
+
+type Major = {
+    id: number;
+    name: string;
+    schoolId: number;
+    applicantCount: number;
+    enrollmentCount: number;
+    acceptScore: number;
+    admissionCount: number;
+}
+
+type UserInfo = {
+    userId: number;
+    tableId: null;
+    name: string;
+    realname: string;
+    identityId: string;
+    gender: string;
+    politicalStatus: string;
+    phone: number;
+    source: string;
+    graduateSchool: string;
+    graduateTime: string;
+    isCurrent: boolean;
+    isScience: boolean;
+    school: School;
+    major: Major;
+    englishLevel: string;
+    homeAddress: string;
+    nationality: string;
+    birthday: string;
+    avatarName: string;
+    roomId: number;
+    seatId: number;
+    admissionId: null;
+    isConfirmed: boolean;
+    receiver: string;
+    contactAddress: string;
+    zipcode: number;
+    contactNumber: string;
 };
